3b77b22d82f6ce90330a203ff8cfc0036bb8dc0c
hinatha <hinatha0110@gmail.com>
2021-10-09 00:15:39 +0900

    inquery内のメールの送信機能

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
-import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -22,9 +21,51 @@ mail = Mail(app)
 def index():
   return render_template("index.html")
 
-@app.route("/inquery")
+@app.route("/inquery", methods=["GET", "POST"])
 def inquery():
-  return render_template("inquery.html")
+  if request.method == "POST":
+    yourname = request.form.get('yourname')
+    furigana = request.form.get('furigana')
+    recipient = request.form.get('mail')
+    phone = request.form.get('phone')
+    gender = request.form.get('gender')
+    choice = request.form.get('choice')
+    contents = request.form.get('contents')
+    
+    serv_mail = Message('問い合わせがきました。', recipients=[os.environ.get('MAIL_DEFAULT_SENDER')])
+    serv_mail.body = ('お問合せ内容確認')
+    serv_mail.html = ('<h1>問い合わせ</h1>'
+                '<br>'
+                '<p>名前： {{ yourname }} </p>'
+                '<p>ふりがな： {{ furigana }} </p>'
+                '<p>メールアドレス： {{ recipient }} </p>'
+                '<p>電話番号： {{ phone }} </p>'
+                '<p>性別： {{ gender }} </p>'
+                '<p>問い合わせ項目： {{ choice }} </p>'
+                '<p>問い合わせ内容： {{ contents }} </p>'
+                , yourname, furigana, recipient, phone, gender, choice, contents)
+    mail.send(serv_mail)
+
+    msgr = Message('問い合わせ送信完了しました！', recipients=[recipient])
+    msgr.body = ('お問合せ内容確認')
+    msgr.html = ('<h1>問い合わせ送信完了しました！</h1>'
+                '<br>'
+                '<p>名前： {{ yourname }} </p>'
+                '<p>ふりがな： {{ furigana }} </p>'
+                '<p>メールアドレス： {{ recipient }} </p>'
+                '<p>電話番号： {{ phone }} </p>'
+                '<p>性別： {{ gender }} </p>'
+                '<p>問い合わせ項目： {{ choice }} </p>'
+                '<p>問い合わせ内容： {{ contents }} </p>'
+                , yourname, furigana, recipient, phone, gender, choice, contents)
+    mail.send(msgr)
+    flash('送信完了しました')
+    return redirect("/")
+
+  else:
+    return render_template("inquery.html")
+
+  
 
 @app.route("/company")
 def company():
