81e8701dcfbdbdb73ce98c2eaaa0e7d8b7fe3209
hinatha <hinatha0110@gmail.com>
2021-10-08 11:56:08 +0900

    メール機能の実装

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+mail = Mail(app)
+
 
 @app.route("/")
 def index():
