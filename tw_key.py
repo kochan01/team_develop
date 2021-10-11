4102c672a0e16e74c26b746ba20a4524cd5b6d49
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-10-12 05:52:12 +0900

    ツイートの取得

---
+++ tw_key.py
@@ -0,0 +1,6 @@
+twdict = {
+  "cons_key": "IfSt2tYnW2sMV4LXleDqhu1nm",
+  "cons_sec": "pM2qlPhUjQMtzMb5HgZ6UzEW5OMHXvBAxNiaeL6AMI9LnQI67d",
+  "accto_key": "962088013034434561-Bp76ySKgidqOckLLjWPe4sx4wxeXvHT",
+  "accto_sec": "IZml9I08vi5o5zp8NJdKqVA4jebHDtZ3czJkhvt0okgsq"
+}
