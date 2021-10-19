0ff0fe0503eb01aee47b33707c7ba9f222526633
hinatha <hinatha0110@gmail.com>
2021-10-19 22:36:48 +0900

    todoリストの実装

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import os
 import tweepy
 import tw_key
 from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -10,6 +12,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__, static_folder='./templates/images')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
 app.config['DEBUG'] = True# デバッグモードをTrueにする
 app.config['SECRET_KEY'] = 'top-secret!'
 app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
@@ -19,6 +23,12 @@ app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 mail = Mail(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
 
 #tweepyの設定
 CONSUMER_KEY = tw_key.twdict['cons_key']
@@ -75,3 +85,56 @@ def company():
 @app.route("/recruit")
 def recruit():
   return render_template("recruit.html")
+
+@app.route("/home", methods=["GET","POST"])
+def home():
+  if request.method == "POST":
+    title = request.form.get("title")
+    detail = request.form.get("detail")
+    due = request.form.get("due")
+
+    due = datetime.strptime(due, '%Y-%m-%d')
+    new_post = Post(title=title, detail=detail, due=due)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect("/home")
+  else:
+    posts = Post.query.order_by(Post.due).all()
+    return render_template('home.html', posts=posts)
+
+@app.route("/todo")
+def todo():
+  return render_template('todo.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+
+    return render_template('detail.html', post=post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/home')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/home')
+
+
+
+ 
